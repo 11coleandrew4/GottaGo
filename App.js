1dcd136df46636a9f1aea2cf8fcf78ec238f0799
Cole Berman <11coleandrew4@gmail.com>
2022-12-26 23:44:42 -0800

    fixed png issue

--- App.js
+++ App.js
@@ -15,17 +15,3 @@ import WelcomeScreen from './components/WelcomeScreen';
 export default function App() {
   return <WelcomeScreen />;
 }
-
-{
-  /* // <SafeAreaView style={styles.container}>
-    //   <View style={{ backgroundColor: 'dodgerblue', flex: 1 }} />
-    //   <View style={{ backgroundColor: 'tomato', flex: 1 }} />
-    //   <View style={{ backgroundColor: 'gold', flex: 1 }} />
-    // </SafeAreaView> */
-}
-//const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#E8EAED',
-//   },
-// });
